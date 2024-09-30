4d9d64ba914e3a31b7da38c1eeabca384d4443ed
acharayaP03 <acharyaP03@gmail.com>
2024-09-30 14:53:29 +1000

    initial commit

---
+++ Data aggregator/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("------------------------ Data Aggregator -----------------------------");
+
+
+const string TicketsFolder = @"";
+
+try
+{
+    var ticketsAggregator = new TicketsAggregator(TicketsFolder);
+
+    ticketsAggregator.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An exception occurred. Exception message:: {ex.Message}");
+}
+
+
+Console.WriteLine("Please press any key to ");
+
+
+
+public class TicketsAggregator
+{
+    private readonly string _ticketsFolder;
+
+    public TicketsAggregator(string ticketsFolder)
+    {
+        _ticketsFolder = ticketsFolder;
+    }
+
+
+    public void Run()
+    {
+        throw new NotImplementedException();
+    }
+}
